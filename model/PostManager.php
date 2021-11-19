0d6eaa1702d818b0ffb05c58043f47c3722b7b84
Fredy Mendes <fredymendes6@gmail.com>
2021-11-19 11:32:21 +0100

    mise à jour du fichier PostManager.php

--- model/PostManager.php
+++ model/PostManager.php
@@ -15,11 +15,26 @@ class PostManager extends Manager
     public function getPost($postId)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('SELECT idPosts, idUsers, title, wording, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') 
-        AS creation_date_fr FROM posts WHERE idUsers = ?');
+        $req = $db->prepare('SELECT idPosts, idUsers, title, wording, content, 
+        DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') 
+        AS creation_date_fr FROM posts WHERE idPosts = ?');
         $req->execute(array($postId));
         $posts = $req->fetch();
 
         return $posts;
+    }
+
+    /**mettre addPost */
+    public function getContent($title, $wording, $content)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('INSERT INTO posts(idUsers, title, wording, content, creation_date) 
+        VALUES(:idUsers, :title, :wording, :content, NOW())');
+        $req->execute(array(
+            'idUsers' => 1,
+            'title' => $title,
+            'wording' => $wording,
+            'content' => $content
+        ));
     }
 }
