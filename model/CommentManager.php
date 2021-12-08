512a58ac97f01bd3759a57c846afe7884a6dd609
Fredy Mendes <fredymendes6@gmail.com>
2021-12-08 11:50:31 +0100

    Mise à jour sql de l'espace commentaire

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -6,19 +6,22 @@ class CommentManager extends Manager
     public function getComments($postId)
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT idComments, idUsers, idPosts, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') 
-        AS comment_date_fr FROM comments WHERE idPosts = ? ORDER BY comment_date DESC');
+        $comments = $db->prepare('SELECT comment, pseudo, DATE_FORMAT(comment_date, \'%d/%m/%Y\') 
+        AS comment_date_fr FROM comments INNER JOIN users ON users.idUsers = comments.idUsers WHERE idPosts = ? ORDER BY comment_date DESC');
         $comments->execute(array($postId));
 
         return $comments;
     }
 
-    public function postComment($postId, $author, $comment)
+    public function postComment($postId, $comment)
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('INSERT INTO comments(idPosts, idUsers, comment, comment_date) VALUES(?, ?, ?, NOW())');
-        $affectedLines = $comments->execute(array($postId, $author, $comment));
-
-        return $affectedLines;
+        $comments = $db->prepare('INSERT INTO comments(idUsers, idPosts, comment, comment_date) 
+        VALUES(:idUsers, :idPosts, :comment, NOW())');
+        $comments->execute(array(
+         'idUsers' => $_SESSION['idUsers'],
+         'idPosts' => $postId, 
+         'comment' => $comment
+        ));
     }
 }
