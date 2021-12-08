6af4dde58df8efae80364d12167c1d6f96a93461
Fredy Mendes <fredymendes6@gmail.com>
2021-12-08 11:52:55 +0100

    Mise à jour et ajouts de requète sql pour l'espace admin

--- model/PostManager.php
+++ model/PostManager.php
@@ -6,8 +6,8 @@ class PostManager extends Manager
     public function getPosts()
     {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT idPosts, idUsers, title, wording, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') 
-        AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+        $req = $db->query('SELECT idPosts, idUsers, title, wording, content, DATE_FORMAT(creation_date, \'%d/%m/%Y\') 
+        AS creation_date_fr FROM posts ORDER BY creation_date DESC');
 
         return $req;
     }
@@ -16,7 +16,7 @@ class PostManager extends Manager
     {
         $db = $this->dbConnect();
         $req = $db->prepare('SELECT idPosts, idUsers, title, wording, content, 
-        DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') 
+        DATE_FORMAT(creation_date, \'%d/%m/%Y\') 
         AS creation_date_fr FROM posts WHERE idPosts = ?');
         $req->execute(array($postId));
         $posts = $req->fetch();
@@ -24,17 +24,37 @@ class PostManager extends Manager
         return $posts;
     }
 
-    /**mettre addPost */
     public function getContent($title, $wording, $content)
     {
         $db = $this->dbConnect();
         $req = $db->prepare('INSERT INTO posts(idUsers, title, wording, content, creation_date) 
         VALUES(:idUsers, :title, :wording, :content, NOW())');
         $req->execute(array(
-            'idUsers' => 1,
+            'idUsers' => $_SESSION['idUsers'],
             'title' => $title,
             'wording' => $wording,
             'content' => $content
         ));
+    }
+
+    public function updatePost($id, $title, $wording, $content)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('UPDATE posts SET title = :title, wording = :wording, content = :content WHERE idPosts = :id');
+        $req->execute([
+            'title' => $title,
+            'wording' => $wording,
+            'content' => $content,
+            'id' => $id
+        ]);
+    }
+
+    public function deletePost($id)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('DELETE FROM posts WHERE idPosts = :id');
+        $req->execute([
+            'id' => $id,
+        ]);
     }
 }
