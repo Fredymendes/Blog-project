7b61a2a88f8e201edffdc129ea93eac3fe74c45f
Fredy Mendes <fredymendes6@gmail.com>
2022-01-22 14:47:06 +0100

    création du dossier libraries pour l'autoloader

---
+++ libraries/Autoloader.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+class Autoloader
+{
+    public static function register()
+    {
+        spl_autoload_register([
+           __CLASS__,
+            'autoload'
+        ]);
+    }
+
+    public static function autoload($class)
+    {
+        //récupération de tous le namespace de la classe concernée
+        //retrait de App\
+        $class = str_replace(__NAMESPACE__ . '\\', '', $class);
+
+        // On remplace les \ par des /
+        $class = str_replace('\\', '/', $class);
+
+        $fichier = __DIR__ . '/../' . $class . '.php';
+
+        // Vérification si le fichier existe
+        if (file_exists($fichier)) {
+            require_once $fichier;
+        }
+    }
+}
